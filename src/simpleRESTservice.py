8b56c803784bd64e839b980b11450117b924903e
yucelz <yucelz@gmail.com>
2024-05-30 11:14:43 +0100

    first upload

---
+++ src/simpleRESTservice.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({"message": "Welcome to the API!"}), 200
+
+@app.route('/about')
+def about():
+    return jsonify({"version": "1.0", "author": "Yagmur Ozden"}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
